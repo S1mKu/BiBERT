b182234285aa12e68f2944f84d41812afd050a9f
Simon Kurz <simon.kurz@tu-dortmund.de>
2024-03-25 15:36:17 +0100

    Adjust config files for STS-B task

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -2,10 +2,12 @@ GENERAL_TINYBERT_DIR="../data/checkpoints/bibert/binarized/cola"
 GENERAL_TINYBERT_DIR="../data/checkpoints/bibert/binarized/sst-2"
 GENERAL_TINYBERT_DIR="./results/biBERT/rte/rte/"
 GENERAL_TINYBERT_DIR="./results/biBERT/mrpc/mrpc/"
+GENERAL_TINYBERT_DIR="./results/biBERT/sts-b/sts-b/"
 
 TASK_DIR="../data/datasets/glue"
 TASK_NAME="RTE"
 TASK_NAME="MRPC"
+TASK_NAME="STS-B"
 
 CUDA_VISIBLE_DEVICES=0 python3 eval.py \
             --data_dir $TASK_DIR \
